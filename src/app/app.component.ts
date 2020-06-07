0e24b837210d19ec6b2f2855645560c7d3f687d4
Andres  Monterrosa <felipe.monterrosa@hotmail.com>
2020-06-07 02:41:10 -0500

    SEPARADO POR COMPONENTES

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { ZXingScannerComponent } from '@zxing/ngx-scanner';
+declare let $: JQueryStatic;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('scanner', { static: false })
+  scanner: ZXingScannerComponent;
+
+  datos: string;
+  mostrar: boolean;
+  constructor() {
+    this.mostrar = false;
+  }
+
+  camerasFoundHandler(e) {
+    console.log('camerasfound', e);
+  }
+
+  camerasNotFoundHandler(e) {
+    console.log('cameranotfound', e);
+  }
+
+  scanSuccessHandler(e) {
+    console.log('scanSuccessHandler', e);
+    this.datos = e;
+    this.mostrar = true;
+  }
+
+  // scanErrorHandler(e) {
+  //   console.log('scanErrorHandler', e);
+  // }
+
+  // scanFailureHandler(e) {
+  //   console.log('scanFailureHandler', e);
+  // }
+
+  // scanCompleteHandler(e) {
+  //   console.log('scanCompleteHandler', e);
+  // }
+
+  ngOnInit(): void {}
 }
